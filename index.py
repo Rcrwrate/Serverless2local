c5ee652f91d5a7163d5ec4b7af20ed2c7cbee164
Rcrwrate <46920034+Rcrwrate@users.noreply.github.com>
2022-11-06 09:45:11 +0800

    修复logging复写问题

--- index.py
+++ index.py
@@ -4,24 +4,23 @@ import re
 
 Manager.register("Routes.Pixiv.index")
 
+l = Log("Router", log_level=40).Log()
 
 def main(message):
-    l = Log("Router", log_level=40).Log()
     for i in Manager.Plugin:
         # if i in message["path"]:
         if check(i,message["path"]):
             func = Manager.Plugin[i]
-            return func(message)
-            # try:
-            #     return func(message)
-            # except Exception as err:
-            #     l.error(f"[Router][ERROR]\t\t{func}\t\t{err.args}")
-            #     return {
-            #         "isBase64Encoded": False,
-            #         "statusCode": 500,
-            #         "headers": {"Content-Type": "text/html"},
-            #         "body": "SERVER ERROR"
-            #     }
+            try:
+                return func(message)
+            except Exception as err:
+                l.error(f"[Router][ERROR]\t\t{func}\t\t{err.args}")
+                return {
+                    "isBase64Encoded": False,
+                    "statusCode": 500,
+                    "headers": {"Content-Type": "text/html"},
+                    "body": "SERVER ERROR"
+                }
     return {
         "isBase64Encoded": False,
         "statusCode": 404,
